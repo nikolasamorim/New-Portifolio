8a939d2347588cf680e3b5b7c6b6ad78c6db2d3f
Nikolas Amorim <nikolasgabrielamorim@gmail.com>
2022-10-17 11:22:16 -0300

    UpdatingFinally

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function nightforday() {
     document.body.style.setProperty('--dark-second', '#1F252D');
     document.body.style.setProperty('--dark-three', '#2b2f35')
     document.body.style.setProperty('--font-light', '#f0f0f0');
+    document.body.style.setProperty('--font-second', '#d3d3d3');
     document.getElementById('moon').id = 'none-moon';
     document.getElementById('none-sun').id = 'sun';
 
@@ -31,7 +32,8 @@ function dayfornight() {
     document.body.style.setProperty('--dark-first', '#f3f3f3');
     document.body.style.setProperty('--dark-second', '#e0e0e0');
     document.body.style.setProperty('--dark-three', '#fafafa')
-    document.body.style.setProperty('--font-light', 'rgb(32, 32, 32)');
+    document.body.style.setProperty('--font-light', '#202020');
+    document.body.style.setProperty('--font-second', '#3a3a3a');
     document.getElementById('sun').id = 'none-sun';
     document.getElementById('none-moon').id = 'moon';
 
